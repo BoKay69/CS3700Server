c1257175319954e611c3746573c29a9a758500c3
BoKay69 <zack.ellis132@gmail.com>
2025-02-05 13:03:52 -0700

    Put the code in the correct file: ClientProgram.java

--- Server Program/ClientProgram.java
+++ Server Program/ClientProgram.java
@@ -1,5 +1,20 @@
-package Server Program;
+import java.util.Scanner;
 
 public class ClientProgram {
-    
+    public static void main(String[] args) {
+        // Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for the DNS or IP address
+        System.out.print("Enter the DNS or IP address of the server: ");
+
+        // Read user input as a string
+        String serverAddress = scanner.nextline();
+
+        // Display the entered address
+        System.out.println("You entered: " + serverAddress);
+
+        // Close the scanner
+        scanner.close();
+    }
 }
