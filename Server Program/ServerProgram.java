08ffb186003294cdf2bb56c1e251db758c62b65b
Zavalen <zavalen07@gmail.com>
2025-02-05 13:32:09 -0700

    class for table added

--- Server Program/ServerProgram.java
+++ Server Program/ServerProgram.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
 
+public class table {
+    int itemID;
+    String itemDescription;
+    int unitPrice;
+    int inventory;
+
+}
+
 public class ServerProgram {
     public static void main(String[] args) {
+
     }
 }
+
+
 
