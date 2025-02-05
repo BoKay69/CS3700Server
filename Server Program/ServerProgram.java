b4457a51e3e808be8345102eed53a63876a6ce72
BoKay69 <zack.ellis132@gmail.com>
2025-02-05 12:59:22 -0700

    Added documentation and finished the ServerProgram class

--- Server Program/ServerProgram.java
+++ Server Program/ServerProgram.java
@@ -2,7 +2,20 @@ import java.util.Scanner;
 
 public class ServerProgram {
     public static void main(String[] args) {
-        
+        // Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for the DNS or IP address
+        System.out.print("Enter the DNS or IP address of the server: ");
+
+        // Read user input as a string
+        String serverAddress = scanner.nextline();
+
+        // Display the entered address
+        System.out.println("You entered: " + serverAddress);
+
+        // Close the scanner
+        scanner.close();
     }
 }
 
