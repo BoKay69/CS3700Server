d7093fd745abfec5cd46d9049df6b9227bd91406
BoKay69 <zack.ellis132@gmail.com>
2025-02-05 13:05:14 -0700

    Removed incorrect code from program

--- Server Program/ServerProgram.java
+++ Server Program/ServerProgram.java
@@ -1,21 +1,2 @@
-import java.util.Scanner;
 
-public class ServerProgram {
-    public static void main(String[] args) {
-        // Create a Scanner object to read user input
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt the user for the DNS or IP address
-        System.out.print("Enter the DNS or IP address of the server: ");
-
-        // Read user input as a string
-        String serverAddress = scanner.nextline();
-
-        // Display the entered address
-        System.out.println("You entered: " + serverAddress);
-
-        // Close the scanner
-        scanner.close();
-    }
-}
 
